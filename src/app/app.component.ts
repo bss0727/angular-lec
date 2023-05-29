045f36f7a976f9506176c3df6004b79a943dcb30
bss0727 <tjqjatjr1212@gamil.com>
2023-05-29 20:51:15 +0900

    component & module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,10 @@
 import { Component } from '@angular/core';
 
-const array : Array<string> = ['data0','data1','data2']
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title : string = 'firstStudy';
-  
-  showArray : Array<string>;
-
-  constructor (){  
-    this.showArray = array; 
-  }
-
-  public clickAfterPrint () : void{
-    this._innerFunc();
-    console.log(array);
-    console.log(this.title);
-  }
-
-  private _innerFunc(){
-    array.push('data' + array.length);  
-  }
+    visible : boolean = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
+import { WorldComponent } from './world/world.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent
+    HelloComponent,
+    WorldComponent
   ],
   imports: [
     BrowserModule,
