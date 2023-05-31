27cd1550e6df42b199c5adac6b89815dc65ed3a5
bss0727 <tjqjatjr1212@gamil.com>
2023-05-31 17:35:16 +0900

    attribute, construct directive

--- 2nd/src/app/login/login.component.ts
+++ 2nd/src/app/login/login.component.ts
@@ -1,28 +1,45 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
   @Input() visible1! : boolean;
   @Output() sendMyEvent : EventEmitter<any> = new EventEmitter(); 
   id! :string;
   pwd! : string;
+  private message;
 
   constructor() { 
   }
+
   ngOnInit(): void {
   }
+  styleArray = {'wrong_id':false, 'wrong_pwd':false};
 
   tryToLogin() : void{
     if(this.id =='admin' && this.pwd == '1234'){
+      alert('로그인합니다!');
       this.visible1 = true;
-    } else {
-      this.visible1 = false;
-    }
-    console.log(this.id, this.pwd, this.visible1);    
-    this.sendMyEvent.emit(this.visible1);  
+      this.sendMyEvent.emit(this.visible1);  //app컴포넌트에 전달
+    } else if(this.id != 'admin'){
+      this.setMessage = 'wrong id';
+      this.styleArray.wrong_id = true;
+      this.styleArray.wrong_pwd = false;
+    } else if(this.pwd != '1234'){
+      this.setMessage = 'wrong pwd';
+      this.styleArray.wrong_id = false;
+      this.styleArray.wrong_pwd = true;
+    } 
+  }
+
+  set setMessage(arg){ 
+    this.message = arg;
+  } 
+
+  get getMessage() : any{ 
+    return this.message;
   }
 }
